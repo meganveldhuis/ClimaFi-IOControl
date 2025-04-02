1cc3130193f8b931dec7005cec9f95dfccb92bc7
Megan Veldhuis <megan.veldhuis@gmail.com>
2025-04-02 14:52:36 -0600

    add comment

--- src/IOControl.h
+++ src/IOControl.h
@@ -1,6 +1,11 @@
 #ifndef IOCONTROL_H
 #define IOCONTROL_H
 
+/*
+  IOControl.h - Library for Input/Output Control for climate systems.
+  Created by Megan Veldhuis, April 2, 2025.
+*/
+
 #include "LittleFS.h"
 #include "ArduinoJson.h"
 #include <vector>
